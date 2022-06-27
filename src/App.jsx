e9f66f4d9edb72620fd23f804605355550d7bc2b
andressario <106826073+andressario@users.noreply.github.com>
2022-06-26 20:53:16 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import Hello from "./Hello";
 import "./App.css";
 
 function App(){
+
+
 	return(
         <div>
-            Hello wordl
-            <Hello name="Flor" />
+           <h1> Premium Plans</h1>
+           <p className="rosa">Sample text. Lorem ipsum dolr sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+
+        <Hello />
         </div>
     );
+
+    
 }
 export default App;
